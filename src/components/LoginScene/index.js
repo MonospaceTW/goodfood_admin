fa26a5a36e4103c0a33e724cd84b9bb59e145dbd
Tomas Lin <horsekit1982@gmail.com>
2018-08-17 21:38:31 +0800

    #1 Login ErrorMsg 顯示

--- src/components/LoginScene/index.js
+++ src/components/LoginScene/index.js
@@ -1,7 +1,59 @@
 import React, { Component } from 'react';
 import propTypes from 'prop-types';
-import { Form, Label } from 'semantic-ui-react'
+import logo from '../../logo.svg';
+import { Form, Label, Container, Image } from 'semantic-ui-react'
 
+export const checkEmail = v => {
+  if (v === '') {
+    return {
+      emailErr: true,
+      emailErrMsg: 'Email不能為空'
+    };
+  }
+
+  const re = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if (!re.test(v)) {
+    return {
+      emailErr: true,
+      emailErrMsg: 'Email格式錯誤'
+    };
+  }
+
+  return {
+    emailErr: false,
+    emailErrMsg: ''
+  };
+};
+
+export const checkPassword = v => {
+  if (v === '') {
+    return {
+      passwordErr: true,
+      passwordErrMsg: '密碼不能為空'
+    };
+  }
+
+  if (v.length < 8) {
+    return {
+      passwordErr: true,
+      passwordErrMsg: '密碼長度至少要8個字元'
+    };
+  }
+
+  return {
+    passwordErr: false,
+    passwordErrMsg: ''
+  };
+};
+
+
+const ErrorMessage = ({isError, message}) =>
+  isError &&
+  (
+    <Label basic pointing style={{color: 'red'}}>
+      {message}
+    </Label>
+  )
 class LoginScene extends Component {
   static propTypes = {
     handleLogin: propTypes.func.isRequired
@@ -10,6 +62,8 @@ class LoginScene extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      emailErr: false,
+      passwordErr: false,
       emailErrMsg: '',
       passwordErrMsg: '',
       email: '',
@@ -18,6 +72,7 @@ class LoginScene extends Component {
   }
 
   handleInputChange = (event) => {
+    const {email, password} = this.state;
     const target = event.target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
     const name = target.name;
@@ -25,68 +80,80 @@ class LoginScene extends Component {
     this.setState({
       [name]: value
     });
+    
+    this.verifyFields(name, value);
   }
 
-  verifyFields = ({email, password}) => {
-    if(email === '') return {
-      isVerify: false,
-      error: {
-        emailErrMsg: 'Email 不可為空'
-      }
-    };
-
-    if(password === '') return {
-      isVerify: false,
-      error: {
-        passwordErrMsg: 'Password 不可為空'
-      }
-    };
-
-    return {isVerify: true};
+  verifyFields = (name, value) => {
+    switch (name) {
+      case 'email':
+        this.setState(checkEmail(value));
+        break;
+      case 'password':
+        this.setState(checkPassword(value));
+        break;
+      default:
+        break;
+    }
   };
 
-  handleSubmit = () => {
-    const {isVerify, error} = this.verifyFields(this.state);
+  disabledButton = () => {
+    const {emailErr, passwordErr, email, password} = this.state;
 
-    isVerify
-      ? this.props.handleLogin(this.state)
-      : this.setState(error);
+    return emailErr
+      || passwordErr
+      || email === ''
+      || password === '';
   }
+
+  handleSubmit = () =>
+    this.props.handleLogin(this.state)
+
+  emailField = () =>
+    <Form.Field>
+      <Form.Input
+        name='email'
+        error={this.state.emailErrMsg !== ''}
+        placeholder='Email'
+        onChange={this.handleInputChange}
+        value={this.state.email} />
+      <ErrorMessage
+        message={this.state.emailErrMsg}
+        isError={this.state.emailErrMsg !== ''}/>
+    </Form.Field>
+  
+  passwordField = () =>
+    <Form.Field>
+      <Form.Input
+        name='password'
+        error={this.state.passwordErrMsg !== ''}
+        onChange={this.handleInputChange}
+        value={this.state.password}/>
+      <ErrorMessage
+        message={this.state.passwordErrMsg}
+        isError={this.state.passwordErrMsg !== ''}/>
+    </Form.Field>
+
+  loginButton = () =>
+    <Form.Field>
+      <Form.Button
+        primary
+        disabled={this.disabledButton()}
+        onClick={this.handleSubmit} >
+        登入
+      </Form.Button>
+    </Form.Field>
 
   render() {
     return (
-      <div>
+      <Container  text className='p-1'>
+        <Image src={logo}  centered />
         <Form>
-          <Form.Field>
-            <Form.Input
-              name='email'
-              error={this.state.emailErrMsg !== ''}
-              placeholder='Email'
-              onChange={this.handleInputChange}
-              value={this.state.email} />
-            <Label basic pointing style={{color: 'red'}}>
-              {this.state.emailErrMsg}
-            </Label>
-          </Form.Field>
-          <Form.Field>
-            <Form.Input
-              name='password'
-              error={this.state.passwordErrMsg !== ''}
-              onChange={this.handleInputChange}
-              value={this.state.password}/>
-            <Label basic pointing style={{color: 'red'}}>
-              {this.state.passwordErrMsg}
-            </Label>
-          </Form.Field>
-          <Form.Field>
-            <Form.Button  onClick={this.handleSubmit}>
-              登入
-            </Form.Button>
-            
-          </Form.Field>
+          {this.emailField()}
+          {this.passwordField()}
+          {this.loginButton()}
         </Form>
-        <h1>Login Scene</h1>
-      </div>
+      </Container>
     );
   }
 }
