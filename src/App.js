d2f0c2ad0683c540353a50fd93e451a8e3896280
curt <curt6815463@gmail.com>
2018-08-24 17:00:34 +0800

    app.js componentDidMount getOrderList

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import LoginScene from './components/LoginScene';
 import MainScene from './MainScene';
-import {fetchPOST} from './apis/libs/fetch';
+import {fetchPOST,fetchGET} from './apis/libs/fetch';
 import 'semantic-ui-css/semantic.min.css';
 
 class App extends Component {
@@ -14,10 +14,33 @@ class App extends Component {
       },
       routes: {
         currentPage: 'Home'
+      },
+      orderListData:{
+        items:'',
+        paging:''
       }
     };
   }
 
+  componentDidMount(){
+    this.getOrderList()
+  }
+
+  setOrderListData = (updatedOrderListData) =>{
+    this.setState({
+      orderListData:{
+        items:updatedOrderListData.items,
+        paging:updatedOrderListData.paging
+      }
+    })
+  }
+  getOrderList = async () => {
+    const {success,result} = await fetchGET('/groups')
+
+    const updatedData = {items: result.data.items,paging: result.data.paging}
+
+    this.setOrderListData(updatedData)
+  }
 
   setLoginState = (updatedAuthData) =>
     this.setState({auth: {...this.state.auth, ...updatedAuthData}});
@@ -27,7 +50,7 @@ class App extends Component {
 
   handleLogin = async (payload) => {
     const {success} = await fetchPOST('/login', payload);
-    
+
     const updatedData = success ? {isLogin: true} : {message: '登入失敗'};
 
     this.setLoginState(updatedData);
@@ -38,7 +61,7 @@ class App extends Component {
       currentPage={this.state.routes.currentPage}
       changePage={this.changeCurrentPage}
       logout={() => this.setLoginState(false)} />
-  
+
   LoginScene = () =>
     <LoginScene handleLogin={this.handleLogin}/>
 
--- src/components/OrderListScene/index.js
+++ src/components/OrderListScene/index.js
@@ -1,6 +1,27 @@
 import React, {Component} from 'react';
 import {Icon, Label, Menu, Table} from 'semantic-ui-react'
 
+// const OrderListScene = () =>{
+//   <Table celled>
+//     <Table.Header>
+//       <Table.Row>
+//         <Table.HeaderCell>揪團名稱</Table.HeaderCell>
+//         <Table.HeaderCell>餐廳名稱</Table.HeaderCell>
+//         <Table.HeaderCell>揪團時間</Table.HeaderCell>
+//         <Table.HeaderCell></Table.HeaderCell>
+//       </Table.Row>
+//     </Table.Header>
+//
+//     <Table.Body>
+//       <Table.Row>
+//         <Table.Cell></Table.Cell>
+//         <Table.Cell>Cell</Table.Cell>
+//         <Table.Cell>Cell</Table.Cell>
+//         <Table.Cell>Cell</Table.Cell>
+//       </Table.Row>
+//     </Table.Body>
+//   </Table>
+// }
 class OrderListScene extends Component {
 
   // constructor(props){
@@ -29,7 +50,7 @@ class OrderListScene extends Component {
             <Table.Cell>Cell</Table.Cell>
             <Table.Cell>Cell</Table.Cell>
             <Table.Cell>Cell</Table.Cell>
-          </Table.Row>          
+          </Table.Row>
         </Table.Body>
       </Table>
     );
